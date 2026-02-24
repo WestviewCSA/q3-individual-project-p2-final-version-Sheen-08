68becb69b837694c181d4871af17aeb6464ff62f
1962501 <1962501@01-18112.pusd.dom>
2026-02-24 11:37:02 -0800

    helper method for coordinate system done

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -3,15 +3,21 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+ * Worked with Aarna P, Anya Y, Emily S, Purvi J
+ */
+
 public class Runner {
 	
-	public static String[][] mapArray;
-	public static int rows;
+	public static String[][] mapArray; //creates the 2D array to store the map
+	public static int rows; 
 	public static int cols;
-	public static int nums;
+	public static int nums; //number of sections
 	
 	public static void main(String[] args) {
-		readFile("hardMap1"); //change this value to change the text file
+		//change this value to change the text file and run the code
+		//readFile("hardMap1");
+		readQueueFile("hardMap3c");
 	}
 	
 	
@@ -20,7 +26,7 @@ public class Runner {
 			File file = new File(fileName);
 			Scanner scanner = new Scanner(file);
 			
-			
+			//getting the amount of rows, cols, and nums and saving them to variables
 			rows = Integer.parseInt(scanner.next());
 			cols = Integer.parseInt(scanner.next());
 			nums = Integer.parseInt(scanner.next());
@@ -29,10 +35,10 @@ public class Runner {
 				for(int r = 0; r < mapArray.length; r++) {
 					String newRow = scanner.next();
 					for(int c = 0; c < cols; c++) {
-						mapArray[r][c] = newRow.substring(c, c+1);
+						mapArray[r][c] = newRow.substring(c, c+1); //getting the specific value through a substring
 					}
 				}
-			System.out.println(Arrays.deepToString(mapArray));
+			System.out.println(Arrays.deepToString(mapArray)); //printing map
 			scanner.close();
 
 			
@@ -44,19 +50,38 @@ public class Runner {
 	}
 	
 	public static void readQueueFile(String fileName) {
+		File file = new File(fileName);
 		try {
-			File file = new File(fileName);
 			Scanner scanner = new Scanner(file);
 			
-			
+			//getting the amount of rows, cols, and nums and saving them to variables
 			rows = Integer.parseInt(scanner.next());
 			cols = Integer.parseInt(scanner.next());
 			nums = Integer.parseInt(scanner.next());
+			
 			mapArray = new String[rows*nums][cols];
 			
 			
+			while(scanner.hasNext()) {
+				String value = scanner.next();
+				//save the row, col, and num sections to variables
+				int row = Integer.parseInt(scanner.next());
+				int col = Integer.parseInt(scanner.next());
+				int num = Integer.parseInt(scanner.next());
+				//the row location is equal to the row in the file plus the total amount of rows already read
+				
+				mapArray[row + (rows*num)][col] = value;
+				
+			}
+			//iterates through map and fills all null values with a period
 			
-			
+			for(int r = 0; r < mapArray.length; r++) {
+				for(int c = 0; c < cols; c++) {
+					if(mapArray[r][c] == null) {
+						mapArray[r][c] = ".";
+					}
+				}
+			}
 			
 			System.out.println(Arrays.deepToString(mapArray));
 			scanner.close();
