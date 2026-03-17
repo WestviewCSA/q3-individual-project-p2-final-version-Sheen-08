9a8829b2e52493118b0b3a1d00835e0bbbbf0eb9
sheen <sheen@localhost>
2026-03-16 23:08:58 -0700

    Fixed formatting maps and finished queue-based approach for project

--- Q3 Project 1 Period 2/src/Runner.java
+++ Q3 Project 1 Period 2/src/Runner.java
@@ -8,7 +8,7 @@ import java.util.Queue;
 
 
 /*
- * Worked with Aarna P, Anya Y, Emily S, Purvi J
+ * Worked with Aarna P, Anya Y, Emily S
  */
 
 public class Runner {
@@ -21,7 +21,8 @@ public class Runner {
 	public static void main(String[] args) {
 		//change this value to change the text file and run the code
 		try {
-			readFile("easyMap1");	
+			readFile("mediumMap2");	
+			solveQueue("mediumMap2");
 		} catch (IllegalMapCharacterException e){
 			System.out.println(e.getMessage());
 		} catch (IncompleteMapException e) {
@@ -133,14 +134,68 @@ public class Runner {
 		}
 	}
 	
-	public static void Queue() {
-		Queue<ArrayList> queue = new LinkedList<>();
-		
-		for(int i = 0; i < rows; i++) {
-			for(int j = 0; j < rows; j++) {
-				
-			}
-		}
+	public static void solveQueue(String fileName) {
+	    //the queue - AKA. where I need to search
+	    Queue<int[]> queue = new LinkedList<>();
+	    
+	    //keeping track of where I have been so I don't walk in circles
+	    boolean[][] visited = new boolean[rows * nums][cols];
+
+	    //Find Wolverine's start position
+	    int startR = 0;
+	    int startC = 0;
+	    for (int r = 0; r < rows * nums; r++) {
+	        for (int c = 0; c < cols; c++) {
+	            if (mapArray[r][c].equals("w")) {
+	                startR = r;
+	                startC = c;
+	            }
+	        }
+	    }
+
+	    //start searching
+	    
+	    //create a new coordinate pair for the starting point and put it at the end of my queue.
+	    queue.add(new int[]{startR, startC});
+	    visited[startR][startC] = true;
+
+	    while (!queue.isEmpty()) {
+	    	//the .poll() method grabs the item from the front of the queue 
+	    	//and removes it from the queue at the same time so we don't have to use 2 different methods (add and remove)
+	    	//got assistance from google for the .poll() method
+	        int[] current = queue.poll();
+	        int currR = current[0];
+	        int currC = current[1];
+
+	        //see if we found the buck
+	        if (mapArray[currR][currC].equals("$")) {
+	            System.out.println("Found it!");
+	            return; 
+	        }
+
+	        //check North(up), South(down), East(right), West(left)
+	        //check the immediate locations, so one up, one down, one left, one right
+	        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+	        
+	        for (int[] dir : directions) {
+	            int nextR = currR + dir[0];
+	            int nextC = currC + dir[1];
+
+	            //check if it is in the map
+	            if (nextR >= 0 && nextR < rows * nums && nextC >= 0 && nextC < cols) {
+	                //check if it is walkable and hasn't already been visited
+	                if (!mapArray[nextR][nextC].equals("@") && !visited[nextR][nextC]) {
+	                    visited[nextR][nextC] = true;
+	                    queue.add(new int[]{nextR, nextC});
+	                }
+	            }
+	        }
+	    }
+	    // If the list is empty and no coin was found
+	    System.out.println("The Wolverine Store is closed.");
+	}
+	
+	public static void solveStack(String fileName) {
 		
 	}
 
